cfa88e432d4a3d3991cdcd7cc1ecb02e3fa7569f
ProgramDU <bernardinodaniela96@gmail.com>
2024-02-19 11:57:06 -0600

    08 adding google fonts to the AppTheme

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -1,18 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class AppTheme{
-  static const mainColor =Color.fromARGB(7, 163, 89, 147);
-  static const backColor =Color.fromARGB(7, 183, 179, 185);
+  static const mainColor = Color.fromARGB(6, 255, 221, 29);
+  static const backColor = Color.fromARGB(7, 183, 179, 185);
   static final ThemeData lightTheme = ThemeData.light().copyWith(
-    scaffoldBackgroundColor: const Color.fromARGB(255, 74, 114, 179),
+    scaffoldBackgroundColor: const Color.fromARGB(255, 239, 220, 240),
         appBarTheme: const AppBarTheme(color: mainColor),
-        textTheme: const TextTheme(
-          headlineLarge: TextStyle(
-            color: mainColor,
-            fontSize: 20.0,
-            fontFamily: 'sans-serif',
+        textTheme:  TextTheme(
+          headlineLarge: GoogleFonts.montserrat(
+            color: Colors.black,
+            fontSize: 15.0,
+            fontWeight: FontWeight.bold,
+            decoration: TextDecoration.underline,
+            decorationColor: Colors.lightBlue,
+            decorationStyle: TextDecorationStyle.wavy,
+            decorationThickness: 2.0,
+            fontStyle: FontStyle.italic
           ),
 
-        )
+        ),
   ); 
 }
